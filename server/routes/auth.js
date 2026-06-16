20afab87807629b8d6d50801d863d7e7ad351b51
ashritha <ashrithaduppati2@gmail.com>
2026-06-16 18:10:04 +0530

    Fix: use nodemailer gmail service for reliable SMTP config

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,9 +8,7 @@ const { protect } = require('../middleware/auth');
 
 const sendOTPEmail = async (toEmail, userName, otp) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT),
-    secure: false,
+    service: 'gmail',
     auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
   });
   await transporter.sendMail({
